21f8fc13bca75bac3cff19cc7fefce9576149f8c
Mohamed Boussas <mohamedboussas0@gmail.com>
2025-11-19 09:02:32 +0100

    Trailing Zeros

---
+++ Introductory Problems/Trailing Zeros.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    long long zeros = 0;
+    while (n > 0) {
+        n /= 5;
+        zeros += n;
+    }
+
+    cout << zeros << endl;
+    return 0;
+}
+
